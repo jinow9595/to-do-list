cf97dbd7a7b36c3c81d170198d83f67844e170b0
Ji seungmin <jinow9595@gmail.com>
2022-02-20 20:22:44 +0900

    toggle 버튼 text color 변경 추가

--- src/js/bg.js
+++ src/js/bg.js
@@ -1,3 +1,4 @@
+const bodyStyle = document.body.style;
 const bgSearchForm = document.querySelector("#bgSearchForm");
 const searchIcon = bgSearchForm.querySelector("i");
 const inputKeyword = bgSearchForm.querySelector("input");
@@ -11,9 +12,63 @@ bgSearchForm.addEventListener("submit", getRandomBackground);
 searchIcon.addEventListener("click", focusSearchInput);
 toggleDiv.addEventListener("click", changeToggle);
 
-
 function changeToggle(e) {
-    console.log(e.target);
+    const toggle = e.target.getAttribute("value");
+
+    if (toggle === "ON") { // color black -> white
+        const input = document.querySelectorAll("input");
+        const icon = document.querySelectorAll("i");
+        const span = document.querySelectorAll("span");
+
+        bodyStyle.backgroundColor = "black";
+        document.querySelector("footer div span").style.color = "white";
+        document.querySelector("h1").style.color = "white";
+        document.querySelector(".todo-list").style.color = "white";
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].style.color = "white";
+            input[i].style.borderBottom = "2px solid white";
+            input[i].classList.remove("placeholder-black");
+        }
+
+        for (let i = 0; i < icon.length; i++) {
+            icon[i].style.color = "white";
+        }
+
+        for (let i = 0; i < span.length; i++) {
+            span[i].style.color = "white";
+        }
+
+        toggleOn.style.display = "none";
+        toggleOff.style.display = "";
+    } else { // color white -> black
+        const input = document.querySelectorAll("input");
+        const icon = document.querySelectorAll("i");
+        const span = document.querySelectorAll("span");
+
+        bodyStyle.backgroundColor = "white";
+        document.querySelector("footer div span").style.color = "black";
+        document.querySelector("h1").style.color = "black";
+        document.querySelector(".todo-list").style.color = "black";
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].style.color = "black";
+            input[i].style.borderBottom = "2px solid black";
+
+            input[i].classList.add("placeholder-black");
+        }
+
+        for (let i = 0; i < icon.length; i++) {
+            icon[i].style.color = "black";
+        }
+
+        for (let i = 0; i < span.length; i++) {
+            span[i].style.color = "black";
+        }
+
+        toggleOn.style.display = "";
+        toggleOff.style.display = "none";
+    }
 }
 
 function focusSearchInput() {
@@ -22,22 +77,21 @@ function focusSearchInput() {
 
 function getRandomBackground(e) {
     const keyword = inputKeyword.value;
-    const bodyStyle = document.body.style;
 
     bodyStyle.backgroundSize = "cover";
     bodyStyle.backgroundPosition = "center center";
     bodyStyle.backgroundColor = "black";
 
-    // if (keyword) {
-    //     e.preventDefault();
-
-    //     fetch(`https://source.unsplash.com/random/?${keyword}`).then(response => {
-    //         bodyStyle.backgroundImage = `url(${response.url})`
-    //     }).catch(error => {
-    //         console.log(error);
-    //         bodyStyle.backgroundColor = "black";
-    //     });
-    // } else {
-    //     bodyStyle.backgroundImage = `url(https://source.unsplash.com/random/)`;
-    // }
+    if (keyword) {
+        e.preventDefault();
+        fetch(`https://source.unsplash.com/random/?${keyword}`).then(response => {
+            bodyStyle.backgroundImage = `url(${response.url})`
+        }).catch(error => {
+            console.log(error);
+            bodyStyle.backgroundColor = "black";
+        });
+        // bodyStyle.backgroundImage = `url(https://source.unsplash.com/random/?${keyword})`; // 기존에 검색했던 키워드로 재검색 시 이전 검색 배경 노출됨
+    } else {
+        bodyStyle.backgroundImage = `url(https://source.unsplash.com/random/)`;
+    }
 }
